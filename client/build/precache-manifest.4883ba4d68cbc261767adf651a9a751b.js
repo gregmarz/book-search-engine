f3dd6b76d760221c6bc94a4e4e49687db1a0aef3
marzec <gregmarzec02@gmail.com>
2022-08-29 22:15:53 -0400

    added schemas

---
+++ client/build/precache-manifest.4883ba4d68cbc261767adf651a9a751b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b15d24c6ea8704d7980519205bec37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6f63480201daa6a076",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "935c1a34d87b8fed4617",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "ab6f63480201daa6a076",
+    "url": "/static/js/2.8eaa5908.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.8eaa5908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "935c1a34d87b8fed4617",
+    "url": "/static/js/main.641156ba.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.641156ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
