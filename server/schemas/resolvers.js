559f85b9e8e4f6ff5f077aa8220453cf67723713
marzec <gregmarzec02@gmail.com>
2022-08-29 22:47:27 -0400

    updated mutation resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,28 @@
 const { AuthenticationError } = require("apollo-server-express");
+const { async } = require("rxjs");
 const { User, Book } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {},
-  Mutation: {},
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("There is no user with that email");
+      }
+      const correctPass = await User.isCorrectPassword(password);
+      if (!correctPass) {
+        throw new AuthenticationError("Password is incorrect");
+      }
+      const token = signToken(user);
+      return { user, token };
+    },
+  },
 };
 module.exports = resolvers;
